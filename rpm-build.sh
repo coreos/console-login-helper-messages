3abd2cd8b14517622001af0e6aa8f4de1a4843e9
Robert Fairley <rfairley@redhat.com>
2018-09-26 16:22:37 -0400

    add rpmlocalbuild to repo

--- rpm-build.sh
+++ rpm-build.sh
@@ -8,4 +8,4 @@ PKG_VER="0.1"
 mkdir -p $PKG_NAME-$PKG_VER
 cp -r LICENSE README.md usr --target-directory $PKG_NAME-$PKG_VER/
 tar -cvzf $PKG_NAME-$PKG_VER.tar.gz $PKG_NAME-$PKG_VER
-../rpmlocalbuild $PKG_NAME.spec
+./rpmlocalbuild $PKG_NAME.spec
