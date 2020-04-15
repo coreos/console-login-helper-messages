592af451936b4be9c2d586c25467460ac0b70d4f
Robert Fairley <rfairley@redhat.com>
2020-04-15 03:42:16 -0400

    motdgen: do not share a staged file, use mktemp and mv
    With the `staged` file shared, there would be potential for two or
    more processes executing `motdgen` to write to it resulting in
    corrupted output, or the error in the `cat` command due to missing
    file reported in #35 (comment). Currently, this is not a problem with
    motdgen, but could be if `motdgen` were invoked by something like the
    udev rules that invoke `issuegen`. A bug reported in `issuegen` for
    this reason is: https://github.com/coreos/console-login-helper-messages/issues/35
    
    Instead, write the intermediate output to a unique tempfile and mv the
    tempfile to the final output location. If the final output is on the
    same filesystem as the tempfile, this operation should be atomic. This
    ensures only valid output is written to the issue file shown to the
    terminal.
    
    Additionally, perform code tidyups similar to those done for
    `issuegen` in https://github.com/coreos/console-login-helper-messages/pull/40.

---
+++ usr/lib/console-login-helper-messages/libutil.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/bash
+#
+# Collection of util functions and common definitions for
+# console-login-helper-messages scripts.
+
+PKG_NAME="console-login-helper-messages"
+
+tempfile_template="${PKG_NAME}.XXXXXXXXXX.tmp"
+# Use same filesystem, under /run, as where snippets are generated, so
+# that rename operations through `mv` are atomic.
+tempfile_dir="/run/${PKG_NAME}"
+# Default SELinux context at destination is applied, e.g. for sshd which
+# requires that written files in `/run/motd.d` maintain the type
+# `pam_var_run_t`.
+mv_Z="mv -Z"
+
+# Write stdin to a tempfile, and rename the tempfile to the path given
+# as an argument. When called from multiple processes on the same
+# generated file path, this avoids interleaving writes to the generated
+# file by using `mv` to overwrite the file.
+write_via_tempfile() {
+    local generated_file="$1"
+    local staged_file="$(mktemp --tmpdir="${tempfile_dir}" "${tempfile_template}")"
+    cat > "${staged_file}"
+    ${mv_Z} "${staged_file}" "${generated_file}"
+}
+
+# Write concatenation of all files with a given suffix from a list of
+# source directories to a target file. The target file is the first
+# argument; suffix the second; and source directories the remaining,
+# searched in the given order in the list. Atomicity of the write to 
+# the target file is given by appending file contents to a tempfile
+# before moving to the target file.
+cat_via_tempfile() {
+    local generated_file="$1"
+    local filter_suffix="$2"
+    shift 2
+    local source_dirs="$@"
+    local staged_file="$(mktemp --tmpdir="${tempfile_dir}" "${tempfile_template}")"
+    for source_dir in ${source_dirs[@]}; do
+        # Ignore stderr, and let the command succeed if no files are
+        # found in the source directory.
+        cat "${source_dir}"/*"$filter_suffix" 2>/dev/null >> "${staged_file}" || :
+    done
+    ${mv_Z} "${staged_file}" "${generated_file}"
+}
