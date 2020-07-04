60e07b5a107b2eac88c4a648a19df51def8c6def
Robert Fairley <rfairley@redhat.com>
2020-07-04 14:20:35 -0400

    add script to sync source code to VM
    Add a script to execute the `make install` target to a local
    directory via a container, and rsync the directory to the root
    of the virtual machine. This avoids the extra work in building an
    RPM with `install_vm_rpms.sh`, and means a Makefile versioned with
    the source can be used rather than updating a specfile which
    comes from a separate git repository (Fedora SCM).

---
+++ sync_to_vm.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+
+set -xeuo pipefail
+
+pkg=console-login-helper-messages
+vmdir=./vm
+sshkey_path="$vmdir/id_rsa"
+ssh_port="2226"
+ssh_opts="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
+
+# rootfs to install to, mounted in the container
+mkdir -p "./build/rootfs"
+
+podman run --rm --name $pkg -v ./:/$pkg $pkg make install DESTDIR=/$pkg/build/rootfs
+
+# Note: don't use the -a (archive) option for rsync - don't preserve
+# file owner (-o) and groups (-g) (these should all be root), otherwise
+# systemd-tmpfiles appears to run into problems creating the runtime
+# directories. https://github.com/systemd/systemd/issues/11282
+rsync -rlpdv -e "ssh $ssh_opts -i $sshkey_path -p $ssh_port" ./build/rootfs/ root@localhost:/
+
+# Enable applicable units, and reboot the VM so the services start as
+# if installed already.
+ssh $ssh_opts -i $sshkey_path -p $ssh_port root@localhost <<EOF
+set -xeuo pipefail
+systemctl enable console-login-helper-messages-issuegen.path
+systemctl enable console-login-helper-messages-motdgen.path
+systemctl reboot
+EOF
+
+echo SSH into the VM with: ssh -i $sshkey_path -p $ssh_port root@localhost
