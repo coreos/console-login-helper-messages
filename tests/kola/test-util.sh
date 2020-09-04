efab15eb5d3cfae4a39afa9717aad8e6bc38ea84
Kelvin Fan <kfan@redhat.com>
2020-09-04 17:24:12 -0400

    tests/kola: Modify kola tests to adapt to using public runtime dir
    Let Kola tests check whether CLHM is using public or private runtime
    directories and adjust tests accordingly.

--- tests/kola/test-util.sh
+++ tests/kola/test-util.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-# Collection of util functions and common definitions for kola tests
-
-. ${KOLA_EXT_DATA}/libtest.sh
+# Collection of util functions and common definitions for kola tests.
 
 PKG_NAME="console-login-helper-messages"
-# ISSUE_RUN_SNIPPETS, MOTD_RUN_SNIPPETS may be changed in the future once 
-# util-linux 2.36 is available and we can drop issue snippets directly into 
-# /run/issue.d. 
-ISSUE_RUN_SNIPPETS_PATH="/run/${PKG_NAME}/issue.d"
-MOTD_RUN_SNIPPETS_PATH="/run/${PKG_NAME}/motd.d"
+
+.  ${KOLA_EXT_DATA}/libtest.sh
+# Source common definitions for console-login-helper-messages scripts.
+. /usr/lib/${PKG_NAME}/libutil.sh
+
+# ISSUE_RUN_SNIPPETS, MOTD_RUN_SNIPPETS may be different, depending on how
+# console-login-helper-messages is configured. By default, if `util-linux`
+# version is at least 2.36, `USE_PUBLIC_RUN_DIR` is set to "true".
+if [ ${USE_PUBLIC_RUN_DIR} == "true" ]; then
+    # Use public runtime directories
+    ISSUE_RUN_SNIPPETS_PATH="/run/issue.d"
+    MOTD_RUN_SNIPPETS_PATH="/run/motd.d"
+else
+    # Use private runtime directories
+    ISSUE_RUN_SNIPPETS_PATH="/run/${PKG_NAME}/issue.d"
+    MOTD_RUN_SNIPPETS_PATH="/run/${PKG_NAME}/motd.d"
+fi
