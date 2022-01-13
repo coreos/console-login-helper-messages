51eb238b0e9cfb77b5971674cb10b1bbe33adbc3
Timothée Ravier <tim@siosm.fr>
2022-01-13 15:29:41 +0100

    etc & usr: Quotes variables as needed & small fixes

--- tests/kola/basic/test-motd.sh
+++ tests/kola/basic/test-motd.sh
@@ -6,15 +6,16 @@ set -xeuo pipefail
 
 . ${KOLA_EXT_DATA}/test-util.sh
 
-if ! systemctl is-enabled ${PKG_NAME}-gensnippet-os-release.service; then
+unit="${PKG_NAME}-gensnippet-os-release.service"
+if ! systemctl is-enabled "${unit}"; then
   fatal "unit ${unit} not enabled"
 fi
 
 # Check that the OS Release snippet was created.
-assert_has_file ${MOTD_RUN_SNIPPETS_PATH}/21_os_release.motd
+assert_has_file "${MOTD_RUN_SNIPPETS_PATH}/21_os_release.motd"
 ok "gensnippet_os_release"
 
-cd $(mktemp -d)
+cd "$(mktemp -d)"
 
 # Generate SSH keys and add public key to authorized keys
 ssh-keygen -t rsa -N "" -f my.key
@@ -22,7 +23,7 @@ mkdir -p ~/.ssh/
 cat my.key.pub >> ~/.ssh/authorized_keys
 # Check that a new motd snippet will be displayed at login from SSH when a .motd
 # file is dropped into the MOTD run directory.
-echo 'foo' > ${MOTD_RUN_SNIPPETS_PATH}/10_foo.motd
+echo 'foo' > "${MOTD_RUN_SNIPPETS_PATH}/10_foo.motd"
 ( timeout 1 script -c "ssh -tt -o StrictHostKeyChecking=no -i my.key root@localhost" ssh_login_output.txt ) || :
 assert_file_has_content ssh_login_output.txt 'foo'
 ok "display new MOTD"
